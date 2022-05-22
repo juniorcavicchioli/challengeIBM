83132cfe2bed8efe9a408d4726cfb3e5e7c7c9d2
Adilson Roberto Cavicchioli Junior <101985616+juniorcavicchioli@users.noreply.github.com>
2022-05-22 12:55:51 -0300

    Adicionando id para Empresa
    Adicionado um atributo id para a empresa assim como uma maneira de gerar um id único

--- Sprint02Challenge/src/br/com/fiap/challenge/model/Empresa.java
+++ Sprint02Challenge/src/br/com/fiap/challenge/model/Empresa.java
@@ -3,10 +3,14 @@ package br.com.fiap.challenge.model;
 import java.util.ArrayList;
 
 public class Empresa extends Dados {
-
+	private int id = Integer.parseInt("5" + Integer.toString(getNextId()));
 	private String cnpj;
 	private ArrayList<Vaga> vagas;
-
+	
+	private static int nextId = 1; // atributo estatico para gerar o id
+	private static int getNextId() {
+		return nextId++;
+	}
 	public ArrayList<Vaga> getVagas() {
 		return vagas;
 	}
